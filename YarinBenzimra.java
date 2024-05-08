b31ea596d4637d64999876e213d4f2146477a7bc
yarinbenzimra <yarinbenzimra@gmail.com>
2024-05-08 19:15:37 +0300

    Adding new line for YarinBenzimra.java

--- YarinBenzimra.java
+++ YarinBenzimra.java
@@ -2,6 +2,8 @@ public class YarinBenzimra{
 public static void main(String[] args) {
 for (int i=1;i<=10; i++) {
 System.out.println("Yarin Benzimra");
+System.out.println("Adding new line");
+
 }
 }
 }
